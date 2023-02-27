96017499f7833609f92b2047a781147921181a8d
Naveenmorty <naveenkumar1297g@gmail.com>
2023-02-27 14:58:37 +0530

    Create enable-branch-protection.sh

---
+++ .github/workflows/enable-branch-protection.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+OAUTH2_TOKEN=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
+
+OWNER=Naveenmorty
+REPO=SampleWar # retrieve this with: basename $(git config --get remote.origin.url) .git
+
+curl https://api.github.com/repos/${OWNER}/${REPO}/branches/master \
+    -H "Authorization: token $OAUTH2_TOKEN" \
+    -H "Accept: application/vnd.github.loki-preview+json" \
+    -X PATCH \
+    -d '{
+      "protection": {
+        "enabled": true,
+        "required_status_checks": {
+          "enforcement_level": "everyone",
+          "contexts": [
+            "default"
+          ]
+        }
+      }
+    }' \
+    -s | json protection
